7b1df9f363e69ac7e9ed8ac4c09970cf7d9f2bc4
nulzi <kimty131@naver.com>
2022-12-27 22:37:18 +0900

    [Update] basic
    React는 사용자 정의 태그를 만드는 기술
    ch4 component
    function 사용
    이름은 대문자로 시작

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,7 +1,50 @@
 import "./App.css";
 
+function Header() {
+  return (
+    <header>
+      <h1>
+        <a href="/">WEB</a>
+      </h1>
+    </header>
+  );
+}
+
+function Nav() {
+  return (
+    <nav>
+      <ol>
+        <li>
+          <a href="/read/1">html</a>
+        </li>
+        <li>
+          <a href="/read/w">css</a>
+        </li>
+        <li>
+          <a href="/read/3">js</a>
+        </li>
+      </ol>
+    </nav>
+  );
+}
+
+function Article() {
+  return (
+    <article>
+      <h2>Welcome</h2>
+      Hello, WEB
+    </article>
+  );
+}
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
+    </div>
+  );
 }
 
 export default App;
