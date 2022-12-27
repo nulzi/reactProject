d104c4c12c2f4ea680ed8bcf08c411528198a814
nulzi <kimty131@naver.com>
2022-12-27 23:02:11 +0900

    [Update] basic
    ch7 state
    입력 : props(외부), state(내부)
    컴포넌트 재렌더링
    useState는 초기값, 값변경함수 반환
    event.taget은 문자열

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState } from "react";
 
 function Header(props) {
   return (
@@ -40,7 +41,7 @@ function Nav(props) {
           href={"/read/" + topic.id}
           onClick={(event) => {
             event.preventDefault();
-            props.onChangeMode(event.target.id);
+            props.onChangeMode(Number(event.target.id));
           }}
         >
           {topic.title}
@@ -65,26 +66,43 @@ function Article(props) {
 }
 
 function App() {
+  const [mode, setMode] = useState("WELCOME");
+  const [id, setId] = useState(null);
   const topics = [
     { id: 1, title: "html", body: "html is ..." },
     { id: 2, title: "css", body: "css is ..." },
     { id: 3, title: "js", body: "js is ..." },
   ];
+  let content = null;
+  if (mode === "WELCOME") {
+    content = <Article title="Welcome" body="Hello, WEB"></Article>;
+  } else if (mode === "READ") {
+    let title,
+      body = null;
+    for (let i = 0; i < topics.length; i++) {
+      if (topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>;
+  }
   return (
     <div>
       <Header
         title="WEB"
         onChangeMode={() => {
-          alert("Header");
+          setMode("WELCOME");
         }}
       ></Header>
       <Nav
         topics={topics}
         onChangeMode={(id) => {
-          alert(id);
+          setMode("READ");
+          setId(id);
         }}
       ></Nav>
-      <Article title="Welcome" body="Hello, WEB"></Article>
+      {content}
     </div>
   );
 }
